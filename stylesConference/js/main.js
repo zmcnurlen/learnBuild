75c0e49b7f86e8caf697fd618d8825c08c0d30ce
zmcnurlen <zachmcnurlen@gmail.com>
2023-01-20 13:40:22 -0700

    initial commit

---
+++ stylesConference/js/main.js
@@ -0,0 +1,64 @@
+// Square the given number x
+function square1(x) {
+  // TODO: complete the function code
+  return (Math.pow(x,2))
+}
+
+ // Square the given number x
+ const square2 = x => (Math.pow(x,2))// TODO: complete the function code
+
+// console.log(square1(0)); // Must show 0
+// console.log(square1(2)); // Must show 4
+// console.log(square1(5)); // Must show 25
+
+
+// console.log(square2(0)); // Must show 0
+// console.log(square2(2)); // Must show 4
+// console.log(square2(5)); // Must show 25
+
+
+function sqre(x){
+  x = Math.pow(x,2)
+  return x
+}
+
+// console.log(sqre(3))
+
+for (i = 1; i <= 15; i++){
+ // console.log(square1(i))
+}
+
+
+// let number;
+// for (number = 1; number <= 20; number++) {
+//   console.log(square2(number));
+// }
+
+
+
+// TODO: write the min() function
+
+function min(x,x,x){
+  minnn = (Math.min(x))
+  return minnn
+}
+
+
+
+console.log(min(5, 4.5,2)); // Must show 4.5
+console.log(min(19, 3, 2)); // Must show 9
+console.log(min(1, 1, .5)); // Must show 
+
+
+
+
+function calculate(x,x,x){
+  
+}
+
+
+
+console.log(calculate(4, "+", 6));  // Must show 10
+console.log(calculate(4, "-", 6));  // Must show -2
+console.log(calculate(2, "*", 0));  // Must show 0
+console.log(calculate(12, "/", 0)); // Must show Infinity
